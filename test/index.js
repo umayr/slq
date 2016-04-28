26c65ed97039dbee1354399df2ca75098e644d80
Umayr Shahid <umayrr@hotmail.co.uk>
2016-04-29 00:49:58 +0500

    Remove an extra argument

--- test/index.js
+++ test/index.js
@@ -18,35 +18,35 @@ describe('slq', () => {
     });
 
     it('should be false when `foo AND bar`', () => {
-      expect(src.query('foo AND bar', false)).to.be.false;
+      expect(src.query('foo AND bar')).to.be.false;
     });
 
     it('should be false when `(foo AND bar) AND baz`', () => {
-      expect(src.query('(foo AND bar) AND baz', false)).to.be.false;
+      expect(src.query('(foo AND bar) AND baz')).to.be.false;
     });
 
     it('should be false when `foo AND (bar AND baz)`', () => {
-      expect(src.query('foo AND (bar AND baz)', false)).to.be.false;
+      expect(src.query('foo AND (bar AND baz)')).to.be.false;
     });
 
     it('should be false when `foo AND (foo AND (bar AND baz))`', () => {
-      expect(src.query('foo AND (foo AND (bar AND baz))', false)).to.be.false;
+      expect(src.query('foo AND (foo AND (bar AND baz))')).to.be.false;
     });
 
     it('should be true when `foo AND (foo AND foo)`', () => {
-      expect(src.query('foo AND (foo AND foo)', true)).to.be.true;
+      expect(src.query('foo AND (foo AND foo)')).to.be.true;
     });
 
     it('should be true when `foo AND (foo OR bar)`', () => {
-      expect(src.query('foo AND (foo OR bar)', true)).to.be.true;
+      expect(src.query('foo AND (foo OR bar)')).to.be.true;
     });
 
     it('should be true when `foo OR (foo AND bar)`', () => {
-      expect(src.query('foo OR (foo AND bar)', true)).to.be.true;
+      expect(src.query('foo OR (foo AND bar)')).to.be.true;
     });
 
     it('should be true when `baz OR (foo AND (foo OR (foo AND (bar OR (foo AND (baz OR foo))))))`', () => {
-      expect(src.query('baz OR (foo AND (foo OR (foo AND (bar OR (foo AND (baz OR foo))))))', true)).to.be.true;
+      expect(src.query('baz OR (foo AND (foo OR (foo AND (bar OR (foo AND (baz OR foo))))))')).to.be.true;
     });
   });
 });
